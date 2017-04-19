749cd4f67e4bb995cb9fce2378478d67b09f2eb1
Michael Desson <mdesson@gmail.com>
2017-04-19 19:57:35 -0400

    Set up skeleton of weather.py. Simplified organization. For real this time.

--- weather.py
+++ weather.py
@@ -6,8 +6,32 @@ hourly = request.urlopen("https://weather.gc.ca/forecast/hourly/qc-147_metric_e.
 
 soup = BeautifulSoup(now_week, "lxml")
 
-with open("soup.txt", 'w') as file:
-    file.write(soup.prettify())
+soup.prettify()
 
 # Now + Weekly: https://weather.gc.ca/city/pages/qc-147_metric_e.html
 # Hourly: https://weather.gc.ca/forecast/hourly/qc-147_metric_e.html
+
+# NOW SECTION:
+# <div class="col-sm-2 brdr-rght  text-center" style="vertical-align: top; min-height: 166px;"><img width="60" height="51" class="center-block mrgn-tp-md" src="/weathericons/12.gif" alt="Light Rain" title="Light Rain"><p class="visible-xs text-center">Light Rain</p>
+#             <div>
+#               <p class="text-center mrgn-tp-md mrgn-bttm-sm lead"><span class="wxo-metric-hide">7°<abbr title="Celsius">C</abbr>
+#                 </span><span class="wxo-imperial-hide wxo-city-hidden">45°<abbr title="Fahrenheit">F</abbr>
+#                 </span>
+#               </p>
+#               <ul class="list-inline list-unstyled text-center wxo-imperial-hide wxo-city-hidden hidden-print">
+#                 <li>
+#                   <a class="wxo-btn-metric-toggle" href="/city/pages/qc-147_metric_e.html" title="Convert to Metric Units">°C</a>
+#                 </li>
+#                 <li class="brdr-lft">°<abbr title="Fahrenheit">F</abbr>
+#                 </li>
+#               </ul>
+#               <ul class="list-inline list-unstyled text-center wxo-metric-hide hidden-print">
+#                 <li>°<abbr title="Celsius">C</abbr>
+#                 </li>
+#                 <li class="brdr-lft">
+#                   <a class="wxo-btn-imperial-toggle" href="qc-147_metric_e.html?unit=imperial" data-link-id="qc-147_metric_e.html?unit=imperial" title="Convert to Imperial Units">°F
+#                         </a>
+#                 </li>
+#               </ul>
+#             </div>
+#           </div>
