761b3afdf2166032da3db91df893c9715f580720
Michael Desson <mdesson@gmail.com>
2017-04-19 23:20:42 -0400

    Wrote first version of bs4 class scraper.(for real this time)

--- weather.py
+++ weather.py
@@ -1,13 +1,24 @@
-from bs4 import BeautifulSoup
 from urllib import request
+from bs4 import BeautifulSoup
 
-now_week = request.urlopen("https://weather.gc.ca/city/pages/qc-147_metric_e.html")
-hourly = request.urlopen("https://weather.gc.ca/forecast/hourly/qc-147_metric_e.html")
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 
-soup = BeautifulSoup(now_week, "lxml")
+week_req = request.Request("https://weather.gc.ca/city/pages/qc-147_metric_e.html", headers=headers)
+# hour_req = request.Request("https://weather.gc.ca/forecast/hourly/qc-147_metric_e.html", headers=headers)
 
-soup.prettify()
+res = request.urlopen(week_req)
+html = res.read()
+soup = BeautifulSoup(html, "lxml")
 
+spans = soup.find_all("span", {'class': "wxo-metric-hide"})
+
+values = []
+
+for span in spans:
+    values.append(span.get_text())
+
+print(values)
 # DESIGN #
-# Find !!! in soup.txt
+# Find !!! in soup.txt. id = "mainContent"
 # For weekly, use text descriptions lower on page
+# 1st span is always current temp
