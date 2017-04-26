9ed3457121989c12a1137691d86d4a0747de44f9
Michael Desson <mdesson@gmail.com>
2017-04-26 19:42:49 -0400

    Updated weather.py to give verbose current weather.

--- weather.py
+++ weather.py
@@ -4,27 +4,39 @@ from bs4 import BeautifulSoup
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 
 week_req = request.Request("https://weather.gc.ca/city/pages/qc-147_metric_e.html", headers=headers)
-# hour_req = request.Request("https://weather.gc.ca/forecast/hourly/qc-147_metric_e.html", headers=headers)
+hour_req = request.Request("https://weather.gc.ca/forecast/hourly/qc-147_metric_e.html", headers=headers)
 
 res = request.urlopen(week_req)
 html = res.read()
 soup = BeautifulSoup(html, "lxml")
 
-spans = soup.find_all("span", {'class': "wxo-metric-hide"})
 
-# values = []
-#
-# for span in spans:
-#     values.append(span.get_text())
-values = [x.get_text() for x in spans]
+def weatherlist(tag, webclass):
+    search = soup.find_all(tag, {"class": webclass})
+    values = [x.get_text().replace('\n','') for x in search]
+    return values
 
-for value in values:
-        print(value)
+# For debug purposes to get all element in webclass.
+def print_tag_list(existing_weatherlist):
+    x = 0
+    for i in existing_weatherlist:
+        print(str(x) + ' ' + i)
+        x +=1
 
-print(values)
-print(soup)
+
+current_weather = weatherlist("dd", "mrgn-bttm-0")
+
+conditions = current_weather[2].lower()
+tendency = current_weather[5].lower()
+temp = current_weather[6]
+wind = current_weather[11]
+humidity = current_weather[10]
+
+
+print("It is {} outside. The temperature is {} and {}. The wind is {} and humidity is at {}."
+      .format(conditions, temp, tendency, wind, humidity))
 
 # DESIGN #
 # Find !!! in soup.txt. id = "mainContent"
-# For weekly, use text descriptions lower on page
-# 1st span is always current temp
+# For verbose weekly, use text descriptions lower on page
+# For brief weekly, use chart below current chart
